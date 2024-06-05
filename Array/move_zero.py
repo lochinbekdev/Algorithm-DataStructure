b67a36fa54b5ebe6a4811fd388d8e2a99a141261
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-06-05 10:19:44 +0500

    selection sort

--- Array/move_zero.py
+++ Array/move_zero.py
@@ -1,10 +1,11 @@
 #  Move Zero solutions
 
-def move_zeros(nums:list)->list:
+def move_zeros(nums): 
     count = 0
     for i,num in enumerate(nums):
         if num == 0:
             count +=1
             continue
-        nums[i],nums[i-count] = nums[i-count],num[i]
-    return num
+        nums[i],nums[i-count] = nums[i-count],nums[i]
+    return nums
+
